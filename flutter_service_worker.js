064389cd5fb346ee49f1c114012dcc7e75726706
Avinash9650 <avinashkmr09xm@gmail.com>
2025-11-17 15:03:29 +0530

    webze web app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -69,7 +69,7 @@ const RESOURCES = {"assets/asset/image/b1.jpg": "5d68052dc9aab694cc2de36e17af039
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "071b4de9340479246a9948eea60442c8",
+"flutter_bootstrap.js": "0a83591c2a697c0f1809f85ba952a4bf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
